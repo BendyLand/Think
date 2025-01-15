497e93bfe2fc6a43163724b142b573a4734d9de5
BendyLand <128928536+BendyLand@users.noreply.github.com>
2025-01-14 22:00:38 -0500

    Begin project structure

---
+++ test/main.cpp
@@ -0,0 +1,13 @@
+#include "mystl.hpp"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "Hello Think!" << endl;
+
+
+    return 0;
+}
+
---
+++ test/premake5.lua
@@ -0,0 +1,29 @@
+-- premake5.lua generated from the Full_build table in blmake.lua!
+
+workspace "Full_build" 
+configurations { "Debug", "Release" } 
+location "build" 
+
+project "Full_build" 
+kind "ConsoleApp" 
+language "C++" 
+targetdir "build" 
+objdir "build/obj" 
+
+-- Compiler and files
+files { "main.cpp" } 
+
+-- Language standard
+cppdialect "C++20" 
+
+-- Build options
+filter "configurations:Debug" 
+    defines { "DEBUG" } 
+    symbols "On"
+    optimize "Off"
+
+filter "configurations:Release"
+    defines { "NDEBUG" } 
+    symbols "Off"
+    optimize "Full" 
+    flags { "LinkTimeOptimization" } 
